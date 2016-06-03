a9b36e60226222abcc99035d68e92ca8bea2d338
oschebell <oschebell@gmail.com>
2016-06-04 01:40:36 +1000

    Modify routes

--- myapp.rb
+++ myapp.rb
@@ -5,14 +5,18 @@ class Myapp < Roda
   plugin :static, ["/images", "/css", "/js"]
   plugin :render
   plugin :head
+
+
+
   route do |r|
+
     r.root do
       view("homepage")
     end
 
-  r.get "done" do
-      view("done")
-    end
+    r.get "done" do
+        view("done")
+      end
 
     r.get "could_do" do
       view("could_do")
